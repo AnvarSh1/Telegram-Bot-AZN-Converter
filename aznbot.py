de5aa923686643972b2a2ba4e28655f5033b0e59
AnvarSh1 <anvar.shirinbayli@gmail.com>
2017-10-14 01:50:26 +0400

    Update aznbot.py

--- aznbot.py
+++ aznbot.py
@@ -15,8 +15,8 @@ PORT = int(os.environ.get('PORT', '5000'))
 updater = Updater(TOKEN)
 # add handlers
 updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
+		port=PORT,
+		url_path=TOKEN)
 updater.bot.set_webhook("https://aznbot.herokuapp.com/" + TOKEN)
 updater.idle()
 
